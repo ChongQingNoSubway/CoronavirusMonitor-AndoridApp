55ad6ee985920621e53ddc5fee5fd6c4f02a6cf0
ChongQingNoSubway <zhuwenhiu@gmail.com>
2021-02-18 13:46:22 -0700

    heatm

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,12 +10,47 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.view.LayoutInflater;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onResume(){
+        super.onResume();
 
+        String number = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("LAST_MEASURE", "0");
+        if (number!="0") {
+
+            TextView tv = (TextView) findViewById(R.id.number1);
+            tv.setText(number);
+//
+            RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
+            tv = (TextView) findViewById(R.id.text);
+            if (Double.parseDouble(number) > 90) {
+                //Cosa mala
+                rb.setRating(0);
+                tv.setText("Your heart rate is to high");
+            } else {
+                //Cosa buena
+                rb.setRating(1);
+                tv.setText("Your heart rate is correct");
+            }
+        }
+
+    }
 
 
 
@@ -27,12 +62,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+        LayoutInflater factory = LayoutInflater.from(MainActivity.this);
+        View fg_one = factory.inflate(R.layout.fragment_first, null);
+        final Button button = (Button) fg_one.findViewById(R.id.heartRate);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent;
+                intent = new Intent(getApplicationContext(), HeartMeasure.class);
+                startActivity(intent);
             }
         });
     }
@@ -60,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 
 }
